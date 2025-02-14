a05053669f2bfdb2cf95d18c5a86399bee00f11e
Vinícius Araújo <d2022003334@unifei.edu.br>
2025-02-14 16:52:28 -0300

    Finalizando a interface gráfica

--- lib/models/memory.dart
+++ lib/models/memory.dart
@@ -2,7 +2,15 @@ class Memory {
   String _value = '0';
 
   void applyCommand(String command) {
-    _value += command;
+    if(command  == 'AC') {
+      _allClear();
+    } else {
+      _value += command;
+    }
+  }
+
+  void _allClear () {
+    _value = '0';
   }
 
   String get value {
--- lib/screens/keyboard.dart
+++ lib/screens/keyboard.dart
@@ -11,6 +11,7 @@ class Keyboard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      color: Color.fromRGBO(48, 48, 48, 1),
       height: 500,
       child: Column(
         children: <Widget> [
@@ -19,24 +20,29 @@ class Keyboard extends StatelessWidget {
               Button(text: '%', cb: cb,color: Button.DARK,),
               Button(text: '/', color: Button.OPERATION, cb: cb),
             ] ),
+            SizedBox(height: 1,),
             ButtonRow( [
               Button(text: '7', cb: cb),
               Button(text: '8', cb: cb),
               Button(text: '9', cb: cb),
               Button(text: 'X', color: Button.OPERATION, cb: cb),
             ] ),
+            SizedBox(height: 1,),
+
             ButtonRow( [
               Button(text: '4', cb: cb),
               Button(text: '5', cb: cb),
               Button(text: '6', cb: cb),
               Button(text: '-', color: Button.OPERATION, cb: cb),
             ] ),
+            SizedBox(height: 1,),
             ButtonRow( [
               Button(text: '1', cb: cb),
               Button(text: '2', cb: cb),
               Button(text: '3', cb: cb),
               Button(text: '+', color: Button.OPERATION, cb: cb),
             ] ),
+            SizedBox(height: 1,),
             ButtonRow( [
               Button(text: '0', big: true, cb: cb),
               Button(text: '.', cb: cb),
