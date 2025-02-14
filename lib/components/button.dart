e07d0f78f2976ddb3ab4b547afc0a58f64c54cc0
Vinícius Araújo <d2022003334@unifei.edu.br>
2025-02-14 16:17:54 -0300

    Definindo as cores no button e mudando o estilo do texto

--- lib/components/button.dart
+++ lib/components/button.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
 
 class Button extends StatelessWidget {
+  static const DARK = Color.fromRGBO(82, 82, 82, 1);
+  static const DEFAULT = Color.fromRGBO(112, 112, 112, 1);
+  static const OPERATION = Color.fromRGBO(250, 158, 13, 1);
+  
   final String text;
   final bool big;
+  final Color color;
 
     Button({
     Key? key,
     required this.text,
     this.big = false,
+    this.color = DEFAULT,
     
   }) : super(key: key);
 
@@ -17,11 +23,23 @@ class Button extends StatelessWidget {
       flex: big ? 2 : 1,
       child: ElevatedButton(
         onPressed: () {},
-        child: Text(text),
+        child: Text(
+          text,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 32,
+            fontWeight: FontWeight.w200,
+
+          ),
+          ),
+        
+        
+        
         style: ElevatedButton.styleFrom(
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(0),
-          ),
+        ),
+        
         ),
         
       ),
